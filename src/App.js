5e54f2a88ba9f7d6259cf335f3c07eb71570e6ed
Yuliya <yulia.podd@gmail.com>
2021-02-27 17:01:11 +0200

    feat: uninstall material-ui

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 
-
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+    <div className="wrapper">
+        <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
