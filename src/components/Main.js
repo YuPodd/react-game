faa007904ef49a2b3849b99cbe47f2bb6dfb6806
Yuliya <yulia.podd@gmail.com>
2021-03-01 14:07:07 +0200

    feat: add generateNumber function

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,17 @@ import paper from '../assets/paper.png';
 import scissors from '../assets/scissors.png';
 
  class Main extends Component {
-          
+  
+      chooseElement(e){
+        console.log(e.target.className);
+       }
+
+     generateNumber () {
+         const max = 3;
+         const min = 1
+        return  console.log(Math.floor(Math.random()*(max-min+1)+min))
+      }
+   
       render() {
         return (
             <main>
@@ -12,9 +22,9 @@ import scissors from '../assets/scissors.png';
                 <section className="user_wrapper">
               <h4>You:</h4>
               <div className="user_icons_wrapper">
-               <button type="button" value="rock!" className="user_button btn btn-outline-warning"><img src={rock} className="rock" alt="rock"  onClick={(e) => alert(e.target.alt)}/></button>
-               <button type="button" value="paper!" className="user_button btn btn-outline-warning"><img src={paper} className="paper" alt="paper" onClick={(e) => alert(e.target.alt)}/></button>
-               <button type="button" value="scissors!" className="user_button btn btn-outline-warning"><img src={scissors} className="scissors" alt="scissors" onClick={(e) => alert(e.target.alt)} /></button>
+               <button type="button" value="rock" className="user_button btn btn-outline-warning"><img src={rock} className="rock" alt="rock"  onClick={(e) => {this.chooseElement(e); this.generateNumber()}}/></button>
+               <button type="button" value="paper" className="user_button btn btn-outline-warning"><img src={paper} className="paper" alt="paper" onClick={(e) => {this.chooseElement(e); this.generateNumber()}}/></button>
+               <button type="button" value="scissors" className="user_button btn btn-outline-warning"><img src={scissors} className="scissors" alt="scissors" onClick={(e) => {this.chooseElement(e); this.generateNumber()}} /></button>
                </div>
               </section>
               <section className="computer_wrapper">
@@ -30,3 +40,4 @@ import scissors from '../assets/scissors.png';
 }
 
 export default  Main
+
