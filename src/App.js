9a5cf63427beaf3f0c4dadf839d1f8b1837c75f6
Yuliya <yulia.podd@gmail.com>
2021-05-12 12:14:50 +0300

    feat: add dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import{ lightTheme, darkTheme } from "./components/Themes"
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const changeTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  }
   return (
-    <>
-      <Header />
+   <> 
+      <Header changeTheme={changeTheme} />
+      <div style={theme === 'light' ? lightTheme : darkTheme}>
       <Main />
+      </div>
       <Footer />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,25 @@
-import React, { Component } from "react";
+import { useState } from "react";
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <nav className="navbar navbar-light bg-light">
-          <span className="navbar-brand">
-            ROCK PAPER SCISSORS GAME
-          </span>
-          <div className="form-group">
-            <div className="custom-control custom-switch">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                disabled=""
-                id="customSwitch2"
-              ></input>
-              <label className="custom-control-label" htmlFor="customSwitch2">
-                Dark Mode
-              </label>
-            </div>
+export default function Header({changeTheme}) {
+  return (
+    <header>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand">ROCK PAPER SCISSORS GAME</span>
+        <div className="form-group">
+          <div className="custom-control custom-switch">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              disabled=""
+              onClick={changeTheme}
+              id="customSwitch"
+            ></input>
+            <label className="custom-control-label" htmlFor="customSwitch">
+              Dark Mode
+            </label>
           </div>
-        </nav>
-      </header>
-    );
-  }
+        </div>
+      </nav>
+    </header>
+  );
 }
-
-export default Header;
