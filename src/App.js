da9ea40a7c6aa3ee27861c36f0c967347718c899
Yuliya <yulia.podd@gmail.com>
2021-05-12 13:18:33 +0300

    refactor: remove spaces

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import{ lightTheme, darkTheme } from "./components/Themes"
+import { lightTheme, darkTheme } from "./components/Themes";
 
 function App() {
   const [theme, setTheme] = useState("light");
   const changeTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
-  }
+  };
   return (
-    <div style={theme === 'light' ? lightTheme : darkTheme}>
+    <div style={theme === "light" ? lightTheme : darkTheme}>
       <Header changeTheme={changeTheme} />
-     
       <Main />
-      
       <Footer />
-      </div>
+    </div>
   );
 }
 export default App;
